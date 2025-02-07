90c812ebe284891f037a81966b9d5be7e43ba56e
Bala Logesh S <balassmp@gmail.com>
2025-02-07 17:24:26 -0500

    Added data and computed propps

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,41 @@
   <div>
     <Header />
     <div class="container">
-      <Balance />
-      <IncomeExpenses />
-      <TransactionList />
+      <Balance :total="total" />
+      <IncomeExpenses :incomeTotal="incomeTotal" :expenseTotal="expenseTotal" />
+      <TransactionList :transactions="transactions" />
       <AddTransaction />
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
 import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
 import AddTransaction from './components/AddTransaction'
 
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction,
-  }
-}
+import { ref, computed } from 'vue'
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 299.97 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 }
+])
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, t) => acc + t.amount, 0)
+})
+
+const incomeTotal = computed(() => {
+  return transactions.value.filter(t => t.amount > 0).reduce((acc, t) => acc + t.amount, 0).toFixed(2)
+})
+
+const expenseTotal = computed(() => {
+  return transactions.value.filter(t => t.amount < 0).reduce((acc, t) => acc + t.amount, 0).toFixed(2)
+})
 
 </script>
 
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,11 +2,26 @@
     <div class="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p id="money-plus" class="money plus">+$100</p>
+            <p id="money-plus" class="money plus">+${{ incomeTotal }}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p id="money-minus" class="money minus">-$100</p>
+            <p id="money-minus" class="money minus">${{ expenseTotal }}</p>
         </div>
     </div>
 </template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    incomeTotal: {
+        type: Number,
+        required: true
+    },
+    expenseTotal: {
+        type: Number,
+        required: true
+    }
+})
+</script>

24ab0784a24905de572ff8474c4180f60edf3182
Bala Logesh S <balassmp@gmail.com>
2025-02-07 16:59:19 -0500

    Created Basic HTML and styles

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>Hello</div>
+  <div>
+    <Header />
+    <div class="container">
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTransaction />
+    </div>
+  </div>
 </template>
+
+<script>
+import Header from './components/Header'
+import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
+
+export default {
+  components: {
+    Header,
+    Balance,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction,
+  }
+}
+
+</script>
 
 <style></style>
